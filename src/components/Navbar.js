0bb2085beeb4b526e84c2f85fc3c2cb12d3425c8
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2023-03-07 12:08:25 +0100

    :recycle: refacto of the index / app / router and ✨ router completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ export default function Navbar() {
     return(
         <>
             <h1>Menu</h1>
+            <Link to="/">home</Link>
+            <Link to="/page">page</Link>
             <Outlet />
         </>
     )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Page from "./pages/Page";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <HashRouter>
+      <Routes>
+        <Route path="/" exact element={<Navbar />}>
+          <Route index element={<Home />}/>
+          <Route path="/page" element={<Page />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </>
 );
