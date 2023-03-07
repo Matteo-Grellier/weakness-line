1a56f59b37bf485fb6638e30ea40e122299ceb9b
Adrien <adrien.poirier@oui-connect.fr>
2023-03-07 14:23:23 +0100

    added stylised homepage :fire:

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-import { Outlet, Link } from "react-router-dom";
-
 export default function Home() {
-    return(
-        <>
-            <h1>Home</h1>
-            <Link to="/createPresentation">create presentation</Link>
-            <Link to="/openPresentation">open presentation</Link>
-        </>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img src="./logo.ico" alt="Logo" className="w-70 h-70" />
+      <h1 className="text-3xl font-bold mt-10 mb-5">
+        Bienvenue sur WeeknessLine !
+      </h1>
+      <div className="space-y-4">
+        <button className="w-full px-10 py-4 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+          Créer une présentation
+        </button>
+        <button className="w-full px-10 py-4 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+          Ouvrir une présentation
+        </button>
+      </div>
+    </div>
+  );
 }
